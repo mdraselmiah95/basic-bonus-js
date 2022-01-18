8f9d76d73f4b8f716e9a2813f795784328a74141
rasel <russelroy15@gmail.com>
2022-01-18 20:20:37 +0600

    switch statement learning

--- coding_challenge.js
+++ coding_challenge.js
@@ -12,3 +12,6 @@ GOOD LUCK 😀
 
 const bill = 275;
 const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+const result = `The bill was ${bill}, the tip was ${tip}, and the total value is ${
+  bill + tip
+}`;
