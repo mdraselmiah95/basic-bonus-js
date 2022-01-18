0bec95cb1989d8860b3157aad2c7a31597096d9d
rasel <russelroy15@gmail.com>
2022-01-18 20:22:54 +0600

    switch statement learning

--- coding_challenge.js
+++ coding_challenge.js
@@ -10,7 +10,7 @@ HINT: Value X is between 50 and 300, if it's >= 50 && <= 300 😉
 GOOD LUCK 😀
 */
 
-const bill = 275;
+const bill = 320;
 const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
 const result = `The bill was ${bill}, the tip was ${tip}, and the total value is ${
   bill + tip
