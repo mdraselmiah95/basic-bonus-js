c6e3939499591357655f9cb6c052a4a127c46ee2
rasel <russelroy15@gmail.com>
2022-01-18 19:40:42 +0600

    switch statement learning

--- switch_statement.js
+++ switch_statement.js
@@ -29,6 +29,7 @@ if (day === "monday") {
   console.log("Plan course structure");
   console.log("Go to coding meetup");
 } else if (day === "tuesday") {
+  console.log("Prepare theory videos");
 } else if (day === "wednesday" || day === "thursday") {
 } else if (day === "friday") {
 } else if (day === "saturday" || day === "sunday") {
